3e960ba806384b629da80ac58688bf245c4b126a
till <till@php.net>
2012-04-12 18:20:56 +0200

    remove quote

--- var/session.sql
+++ var/session.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `session2` (
 `session_data` text NOT NULL,
 `user_id` int(10) DEFAULT NULL,
 `rec_dateadd` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-`rec_datemod` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',"
+`rec_datemod` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
 PRIMARY KEY (`session_id`),
 KEY `gc` (`rec_datemod`),
 KEY `user_idx` (`user_id`)
