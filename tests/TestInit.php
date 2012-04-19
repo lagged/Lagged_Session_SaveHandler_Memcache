7bc7ec0aa178f470bf253f99d6754e1219c24007
till <till@php.net>
2012-04-19 22:40:34 +0200

    * composer moved autoloaders

--- tests/TestInit.php
+++ tests/TestInit.php
@@ -1,5 +1,5 @@
 <?php
-$autoload = dirname(__DIR__) . '/vendor/.composer/autoload.php';
+$autoload = dirname(__DIR__) . '/vendor/autoload.php';
 if (!file_exists($autoload)) {
     die("Please run 'php composer.phar install'.");
 }
