2e33a19d6f9e681afa5139330e546d6a19575450
till <till@php.net>
2012-04-20 19:04:46 +0200

    before script

---
+++ scripts/travis-deps.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+MEMCACHE_VERSION=2.2.6
+
+install_composer() {
+    wget http://getcomposer.org/composer.phar && php composer.phar install
+}
+
+install_ext_memcache() {
+    wget "http://pecl.php.net/get/memcache-${MEMCACHE_VERSION}.tgz"
+    tar -xzf "memcache-${MEMCACHE_VERSION}.tgz"
+    sh -c "cd memcache-${MEMCACHE_VERSION} && phpize && ./configure --enable-memcache && make && sudo make install"
+    echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+}
+
+install_composer
+install_ext_memcache
